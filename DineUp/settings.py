c8b91399c30c5e3068c9d1ad5779a390561738cc
Pollinate IQ <pollinateiq@gmail.com>
2024-10-14 20:21:37 +0200

    Man comon

--- DineUp/settings.py
+++ DineUp/settings.py
@@ -111,8 +111,8 @@ AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage', 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
 
 #Media Files
 MEDIA_URL = '/media/'
